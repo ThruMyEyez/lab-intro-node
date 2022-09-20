ed906b66fa3f96eb3b882dd3fb524a9286fc2fd0
Artur Baumeister <Artur.baumeister@gmail.com>
2022-09-20 21:00:32 +0200

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort();
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    this.length = this.items.length;
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    this.length = this.items.length;
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    this.length = this.items.length;
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    this.length = this.items.length;
+    if (this.length === 0) 0;
+    return this.items.reduce((a, b) => {
+      return a + b;
+    }, 0);
+  }
 
   avg() {}
 }
-
+const test = new SortedList();
 module.exports = SortedList;
+test.add(2);
+test.add(3);
